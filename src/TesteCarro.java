63eccff0f3288ea8c198ccd2984c0dfde0e680f8
Pedro Zardo <pedro.zardo2907@udesc.edu.br>
2025-09-07 17:13:05 -0300

    Adicionado os testes das classes criadas anteriormente

---
+++ src/TesteCarro.java
@@ -0,0 +1,24 @@
+package exercicio1oo.testes;
+
+import exercicio1oo.classes.Carro;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+        // Instanciação da classe Carro
+        Carro carro = new Carro();
+
+        // Atribuição de valores aos atributos
+        carro.marca = "Volkswagen";
+        carro.modelo = "Fusca";
+        carro.ano = 1970;
+
+        // Teste dos métodos e atributos
+        System.out.println("--- Testando a classe Carro ---");
+        System.out.println("Marca: " + carro.marca);
+        System.out.println("Modelo: " + carro.modelo);
+        System.out.println("Ano: " + carro.ano);
+        carro.ligar();
+        carro.desligar();
+        System.out.println("------------------------------\n");
+    }
+}
